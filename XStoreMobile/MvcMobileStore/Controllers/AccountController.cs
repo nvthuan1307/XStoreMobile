958ab1756bd7dcd5c9330b4666b0ae721a6d40a8
tuantuchautrinh <tuantuchautrinh@gmail.com>
2017-12-27 15:55:58 +0700

    change accountcontroler.cs 2

--- XStoreMobile/MvcMobileStore/Controllers/AccountController.cs
+++ XStoreMobile/MvcMobileStore/Controllers/AccountController.cs
@@ -92,18 +92,6 @@ namespace MvcMobileStore.Controllers
         //
         // GET: /Account/Manage
 
-        public ActionResult Manage(ManageMessageId? message)
-        {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
-                : message == ManageMessageId.SetPasswordSuccess ? "Your password has been set."
-                : message == ManageMessageId.RemoveLoginSuccess ? "The external login was removed."
-                : "";
-            ViewBag.HasLocalPassword = OAuthWebSecurity.HasLocalAccount(WebSecurity.GetUserId(User.Identity.Name));
-            ViewBag.ReturnUrl = Url.Action("Manage");
-            return View();
-        }
-
         //
         // POST: /Account/Manage
 
