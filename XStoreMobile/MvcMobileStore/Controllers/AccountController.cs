d8080a5dd8b287daaa992c1db4ca2c171d1fa7bc
tuantuchautrinh <tuantuchautrinh@gmail.com>
2017-12-27 15:54:24 +0700

    change accountcontroler.cs

--- XStoreMobile/MvcMobileStore/Controllers/AccountController.cs
+++ XStoreMobile/MvcMobileStore/Controllers/AccountController.cs
@@ -60,39 +60,6 @@ namespace MvcMobileStore.Controllers
         //
         // GET: /Account/Register
 
-        [AllowAnonymous]
-        public ActionResult Register()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Account/Register
-
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public ActionResult Register(RegisterModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                // Attempt to register the user
-                try
-                {
-                    WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
-                    WebSecurity.Login(model.UserName, model.Password);
-                    return RedirectToAction("Index", "Home");
-                }
-                catch (MembershipCreateUserException e)
-                {
-                    ModelState.AddModelError("", ErrorCodeToString(e.StatusCode));
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
         //
         // POST: /Account/Disassociate
 
